c9c2bc94a00307edb2b9d5cabe4c1f1ac08156a1
andreinfilip <andreinfilip@gmail.com>
2022-03-06 09:53:20 +0200

    Product Details added fixed

--- src/ProductDetails/ProductDetails.js
+++ src/ProductDetails/ProductDetails.js
@@ -11,16 +11,17 @@ function ProductDetails() {
   }
 
   return (
-  <div>
-    <h1>{currentProduct.title}</h1>
-    <p>{currentProduct.description}</p>
-    {currentProduct.image.map((url) => (<img key={url} src={url} alt="product" width="200px" />
-    ))}
-    <p>{currentProduct.price} Lei</p>
-    <p>Stock: {currentProduct.stock}</p>
-    <p>Size: {currentProduct.size}</p>
-    <p>Categories: {currentProduct.categories.join(",")}</p>
-  </div>
+    <div>
+      <h1>{currentProduct.title}</h1>
+      <p>{currentProduct.description}</p>
+      {currentProduct.images.map((url) => (
+        <img key={url} src={url} alt="product" width="200px" />
+      ))}
+      <p>{currentProduct.price} Lei</p>
+      <p>Stock: {currentProduct.stock}</p>
+      <p>Size: {currentProduct.size}</p>
+      <p>Categories: {currentProduct.categories.join(", ")}</p>
+    </div>
   );
 }
 
